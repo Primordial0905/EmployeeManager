0e53510c1f36a347623711f4119a895a7d7b1cb7
Nickholas Destino <nickholas.nl@gmail.com>
2025-02-13 23:33:19 +0700

    Edit bug fix

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -46,37 +46,58 @@ class CreateController extends Controller
         return view('edit',['employee' => $employee]);
     }
 
-    public function EditEmployee(Request $request,$id){
+    public function EditEmployee(Request $request, $id)
+    {
         $employee = Employee::find($id);
+
         $request->validate([
             'Name' => 'required|string|min:5|max:20',
             'Age' => 'required|integer|min:20',
             'Address' => 'required|string|min:10|max:40',
-            'Phone' => 'required|string|min:9|max:12|',
-            'Image' => 'required',
+            'Phone' => 'required|string|min:9|max:12|regex:/^08\d{7,11}$/',
+            'Image' => 'nullable',
         ]);
-        
 
+<<<<<<< HEAD
         unlink(public_path('images/'.$employee->image));
         $file = $request->file('Image');
         $filename = $file->getClientOriginalName();
         $file->move(public_path('images'), $filename);
+=======
+    
+        if ($request->hasFile('Image')) {
+            if ($employee->image && file_exists(public_path('images/' . $employee->image))) {
+                unlink(public_path('images/' . $employee->image)); 
+            }
+>>>>>>> 2cf9b43 (Edit bug fix)
 
-        $employee->update([      
+            $file = $request->file('Image');
+            $filename = $file->getClientOriginalName();
+            $file->move(public_path('images'), $filename);
+        } 
+        else {
+            $filename = $employee->image; 
+        }
+
+        $employee->update([
             'Name' => $request->Name,
             'Age' => $request->Age,
             'Address' => $request->Address,
             'Phone' => $request->Phone,
-            'Image' => $filename
+            'Image' => $filename, 
         ]);
 
-        return redirect('/')->with('success', 'Employee has been added');
-    }
+    return redirect('/')->with('success', 'Employee has been updated');
+}
+
 
     public function DeleteEmployee($id){
         $employee = Employee::find($id);
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2cf9b43 (Edit bug fix)
         unlink(public_path('images/'.$employee->image));
         $employee->delete();
         return redirect('/')->with('success', 'Employee has been deleted');
